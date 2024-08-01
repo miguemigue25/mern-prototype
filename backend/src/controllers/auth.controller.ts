7a482b7a81debca707acb9cd2c6fbdbbcd37e681
Miguel <migueortiz0125@gmail.com>
2024-07-31 19:21:12 -0700

    refresh endpoint added

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,12 +1,12 @@
 import { z } from "zod";
 import catchErrors from "../utils/catchErrors";
-import { createAccount, loginUser } from "../services/auth.service";
-import { CREATED, OK } from "../constants/http";
-import { clearAuthCookies, setAuthCookies } from "../utils/ccokies";
+import { createAccount, loginUser, refreshUserAccessToken } from "../services/auth.service";
+import { CREATED, OK, UNAUTHORIZED } from "../constants/http";
+import { clearAuthCookies, getAccessTokenCookieOptions, getRefreshTokenCookieOptions, setAuthCookies } from "../utils/ccokies";
 import { loginSchema, registerSchema } from "./auth.schemas";
 import { AccessTokenPayload, verifyToken } from "../utils/jwt";
 import SessionModel from "../models/session.models";
-
+import appAssert from "../utils/appAssert";
 
 
 
@@ -41,8 +41,8 @@ export const loginHandler = catchErrors(async (req, res) => {
 });
 
 export const logoutHandler = catchErrors(async (req, res) => {
-    const accessToken = req.cookies.accessToken;
-    const { payload } = verifyToken(accessToken);
+    const accessToken = req.cookies.accessToken as string | undefined;
+    const { payload } = verifyToken(accessToken || "");
 
     if (payload) {
         await SessionModel.findByIdAndDelete(payload.sessionId);
@@ -51,4 +51,24 @@ export const logoutHandler = catchErrors(async (req, res) => {
     return clearAuthCookies(res).status(OK).json({
         message: "Logout successful"
     });
+});
+
+export const refreshHandler = catchErrors(async (req, res) => {
+    const refreshToken = req.cookies.refreshToken as string | undefined;
+    appAssert(refreshToken, UNAUTHORIZED, "Missing refresh token");
+
+    const { accessToken, newRefreshToken } = await refreshUserAccessToken(
+        refreshToken
+    );
+
+    if (newRefreshToken) {
+        res.cookie("refreshToken", newRefreshToken, getRefreshTokenCookieOptions());
+    };
+
+    return res
+        .status(OK)
+        .cookie("accessToken", accessToken, getAccessTokenCookieOptions())
+        .json({
+            message: "Access token refreshed",
+        });
 });
--- backend/src/middleware/errorHandler.ts
+++ backend/src/middleware/errorHandler.ts
@@ -2,6 +2,7 @@ import { ErrorRequestHandler, Response } from "express";
 import { BAD_REQUEST, INTERNAL_SERVER_ERROR } from "../constants/http";
 import { z } from "zod";
 import AppError from "../utils/AppError";
+import { REFRESH_PATH, clearAuthCookies } from "../utils/ccokies";
 
 
 const handleZodError = (res: Response, error: z.ZodError) => {
@@ -25,13 +26,17 @@ const handleAppError = (res: Response, error: AppError) => {
 const errorHandler: ErrorRequestHandler = (error, req, res, next) => {
     console.log(`PATH: ${req.path}`, error);
 
+    if (req.path === REFRESH_PATH) {
+        clearAuthCookies(res);
+    };
+
     if (error instanceof z.ZodError) {
         return handleZodError(res, error);
-    }
+    };
 
     if (error instanceof AppError) {
         return handleAppError(res, error);
-    }
+    };
 
     return res.status(INTERNAL_SERVER_ERROR).send("Internal Server Error");
 };
