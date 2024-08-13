ab24f6c9ec4ee3968ea8c291f8ff3295f2c8d249
Miguel <migueortiz0125@gmail.com>
2024-08-12 22:20:48 -0700

    verify email progress for frontend

---
+++ frontend/src/pages/VerifyEmail.jsx
@@ -0,0 +1,14 @@
+import { Container, Flex } from "@chakra-ui/react";
+
+
+const VerifyEmail = () => {
+    return (
+        <Flex minH="100vh" justify="center" mt={12}>
+            <Container mx="auto" maxW="md" py={12} px={6} textAlign="center">
+
+            </Container>
+        </Flex>
+    )
+}
+
+export default VerifyEmail
