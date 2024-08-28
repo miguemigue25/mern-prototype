faa462a7bc3e4fedf18fbcd52662ecee4418f8cb
Miguel <migueortiz0125@gmail.com>
2024-08-27 22:37:30 -0700

    completed the verify email section

--- frontend/src/pages/VerifyEmail.jsx
+++ frontend/src/pages/VerifyEmail.jsx
@@ -1,11 +1,37 @@
-import { Container, Flex } from "@chakra-ui/react";
+import { Alert, AlertIcon, Container, Flex, Spinner, Text, VStack, Link as ChakraLink } from "@chakra-ui/react";
+import { useQuery } from "@tanstack/react-query";
+import { useParams, Link } from "react-router-dom";
+import { verifyEmail } from "../lib/api";
 
 
 const VerifyEmail = () => {
+    const { code } = useParams();
+    const { isPending, isSuccess, isError } = useQuery({
+        queryKey: ["emailVerification", code],
+        queryFn: () => verifyEmail(code)
+    })
+
     return (
         <Flex minH="100vh" justify="center" mt={12}>
             <Container mx="auto" maxW="md" py={12} px={6} textAlign="center">
-
+                {isPending ? (<Spinner />) : (<VStack align="center" spacing={6}>
+                    <Alert status={isSuccess ? "success" : "error"}
+                        w="fit-content"
+                        borderRadius={12}>
+                        <AlertIcon />
+                        {isSuccess ? "Email verified" : "Invalid link"}
+                    </Alert>
+                    {isError && <Text color='gray.400'>
+                        The link is either invalid or expired.{" "}
+                        <ChakraLink as={Link} to="/register" replace>
+                            Get a new link
+                        </ChakraLink>
+                    </Text>
+                    }
+                    <ChakraLink as={Link} to="/" replace>
+                        Back to home
+                    </ChakraLink>
+                </VStack>)}
             </Container>
         </Flex>
     )
