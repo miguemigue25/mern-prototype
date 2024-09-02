0de178fd667358b4153e47d7afd4a9bb867777ef
Miguel <migueortiz0125@gmail.com>
2024-09-01 22:40:32 -0700

    more progress on auth for fe

--- frontend/src/components/AppContainer.jsx
+++ frontend/src/components/AppContainer.jsx
@@ -1,9 +1,29 @@
+import { Box, Center, Spinner } from "@chakra-ui/react";
+import useAuth from "../hooks/useAuth"
+import { Navigate, Outlet } from "react-router-dom";
 
 
 const AppContainer = () => {
-  return (
-    <div>AppContainer</div>
-  )
-}
+    const { user, isLoading } = useAuth();
 
-export default AppContainer
+    return isLoading ? (
+        <Center w="100vw" h="90vh" flexDir="column">
+            <Spinner mb={4} />
+        </Center>
+    ) : user ? (
+        <Box p={4} minH="100vh">
+            {/* <UserMenu /> */}
+            <Outlet />
+        </Box>
+    ) : (
+        <Navigate
+            to="/login"
+            replace
+            state={{
+                redirectUrl: window.location.pathname
+            }}
+        />
+    );
+};
+
+export default AppContainer;
