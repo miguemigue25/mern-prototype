0538acda7cbe97884814053e9260ebc271ca9015
Miguel <migueortiz0125@gmail.com>
2024-09-05 14:35:14 -0700

    sessions page fe complete

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -7,4 +7,7 @@ export const register = async (data) => API.post("/auth/register", data);
 export const verifyEmail = async (verificationCode) => API.get(`/auth/email/verify/${verificationCode}`);
 export const sendPasswordResetEmail = async (email) => API.post("/auth/password/forgot", { email });
 export const resetPassword = async ({ verificationCode, password }) => API.post("/auth/password/reset", { verificationCode, password });
+
 export const getUser = async () => API.get("/user");
+export const getSessions = async () => API.get("/sessions");
+export const deleteSession = async (id) => API.get(`/sessions/${id}`);
