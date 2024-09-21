30a0e76110c1ba17d075f1c82642e2af37537a74
Miguel <migueortiz0125@gmail.com>
2024-09-20 22:38:37 -0700

    progress so far

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import VerifyEmail from "./pages/VerifyEmail";
@@ -7,15 +7,13 @@ import ResetPassword from "./pages/ResetPassword";
 import AppContainer from "./components/AppContainer";
 import Profile from "./pages/Profile";
 import Settings from "./pages/Settings";
+import { setNavigate } from "./lib/navigation";
 
-
-export const Home = () => {
-  return (
-    <div>Home</div>
-  )
-};
 
 function App() {
+  const navigate = useNavigate();
+  setNavigate(navigate);
+
   return (
     <Routes>
       <Route path="/" element={<AppContainer />} >
--- frontend/src/config/apiClient.js
+++ frontend/src/config/apiClient.js
@@ -1,4 +1,6 @@
 import axios from "axios";
+import queryClient from "./queryClient";
+import { navigate } from "../lib/navigation";
 
 
 const options = {
@@ -6,12 +8,32 @@ const options = {
     withCredentials: true,
 };
 
+const TokenRefreshClient = axios.create(options);
+TokenRefreshClient.interceptors.response.use((response) => response.data);
+
 const API = axios.create(options);
 
 API.interceptors.response.use(
     (response) => response.data,
-    (error) => {
-        const { status, data } = error.response;
+    async (error) => {
+        const { config, response } = error;
+        const { status, data } = response || {};
+
+        // try to refresh the access token behind the scenes
+        if (status === 401 && data?.errorCode === "InvalidAccessToken") {
+            try {
+                await API.get("/auth/refresh");
+                return TokenRefreshClient(config);
+            } catch (error) {
+                queryClient.clear();
+                navigate("/login", {
+                    state: {
+                        redirectUrl: window.location.pathname,
+                    }
+                })
+            }
+        }
+
         return Promise.reject({ status, ...data });
     }
 );
